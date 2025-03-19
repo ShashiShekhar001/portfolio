4e10290500517dd7a46a34fc01ff3e172c648c73
Shashi Shekhar Jha <85563890+ShashiShekhar001@users.noreply.github.com>
2025-03-19 19:53:32 +0530

    Update Conhead.jsx

--- src/components/navbar/conhead/Conhead.jsx
+++ src/components/navbar/conhead/Conhead.jsx
@@ -5,7 +5,7 @@ function Conhead(){
     <div className="contact-head">
         <div className="con-cont">
             <div className="contact-img">
-            <img src="https://lh3.googleusercontent.com/PwsyTNM-FnYGb7m0IEpOk2THRXfjB2VI5ms_4M4sDyE9Vw4hEyZiVjYAo3P_hfmL8ADyRqZKW1UpI-i9LrmQfObFp947jml8HUP-lCAtFtRNjFIE4nk61SSeHVWndjhc7G_tvb82Q-OZIHeotcrMvuXzCzmpX5BnZDBhwMCOMCujxpm0cmLTWi7Ko41XTLTH354LLf0OpBYaFRQ9gKA3J3FUN6p32s3QVc1EXHWUWA_-SQAXqlrZ_w1JicGI0fvE8hCwSevWOUzuvQNY_P1yWDLa_v7cVkL6O6eOzzRl1iMJxiXAldOokjWF-2GbejwfsZSZquhrsoBKWasLee2xuraDpkHvphx7dOBC0UKd9tJfIxg1ov0fV2BcAVbuxQpiBFamlHUfVVvrzXINSvE4YkEISIVGMi_yJVsea8gckoQ3mRbOGsTaFLdegkOIeMQZqzoroBDjhMlaUrzx9GL6TqwqVQZOTdwv8T1agmY3XWePy5yF9vnhPvbrTGKj5lAW9s_mo2hrL2LzqAQzqiw3NLc5H4QK6R4aYSyQMgcFWIaQkvRkB_VPf0Q5TMZzCDgJ8ykuCVFlHhBn4c_DyD7lJJKxWsxKCzSd4j5egmKDoWz2mujyz1DCwJ_l5AFnQDU4ZMzq9_dOXtLqpalpfxssk1yvRAeI3OSk_XV7jcNlb-SeAPx1A9qgcFktMsxzbuMxeMZETuKVje1G5z-UdBNwXd_q0Ej5y382I5qJ8nlW-wwUg4KvqMhMH556j8b-Vz1TiRKKQ0butIWNZ2jntxTjK_ki_lZ21iH1U1-VZ5wKMhO3GK2dm46lQZC5VBw7SUjqQXs=w341-h380-no?authuser=0" alt="" />
+            <img src="https://hackwittechnologies.com/assets/imgs/service/hackwit-marketing.png" alt="Image not available" style={{maxWidth:'500px'}} />
             </div>
             <div className="contact-detail">
                 <h1 className="con-name">Contact Me</h1>
